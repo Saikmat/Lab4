83b6f7e94a8fed80422b287462fd35db18a4a8fa
Saikmat <89786229+Saikmat@users.noreply.github.com>
2023-10-04 09:20:32 -0400

    Added more comments

--- Food.cpp
+++ Food.cpp
@@ -1,5 +1,8 @@
 /*
- *
+ * Name: Sai Matukumalli
+ * Class: CMSY-171
+ * Program name: Lab 4
+ * Program Description: This program tracks the name, inventory, and number of sales per day of a given Food item.
  */
 #include <iostream>
 #include <string>
@@ -13,7 +16,7 @@ const int DEFAULT_NUM_OF_DAYS = 7;
 const std::string DEFAULT_NAME = "None";
 
 /*
- * No Parameter constructor, sets all values to provided defaults
+ * No Parameter constructor, sets all values to defaults
  */
 Food::Food() { // NOLINT(*-pro-type-member-init) // Functions handle value setting so disregard Clang tidy
     setName(DEFAULT_NAME);
@@ -25,6 +28,7 @@ Food::Food() { // NOLINT(*-pro-type-member-init) // Functions handle value setti
 /*
  * Constructor that takes the number of days of sales that are made
  * name and starting inventory is set to default
+ * int numberOfSales - Takes the number of days during which there are sales
  */
 Food::Food(int numberOfSales) { // NOLINT(*-pro-type-member-init)
     // Functions handle value setting so disregard Clang tidy
@@ -62,11 +66,11 @@ void Food::makeArray() {
  * Calculates the ending inventory based on the provided sales
  */
 void Food::calcEnd() {
-    int potentialEnd = inventoryStartCount;
+    int runningEnd = inventoryStartCount;
     for (int i = 0; i < numberOfDaysFoodIsSold; ++i) {
-        potentialEnd -= salesPerDay[i];
+        runningEnd -= salesPerDay[i];
     }
-    setEnd(potentialEnd);
+    setEnd(runningEnd);
 }
 
 /*
@@ -85,9 +89,10 @@ double Food::calcAvg() {
 
 /*
  * Sets the name of the item
+ * string passedName - sets this.name to the value entered
  */
 void Food::setName(string passedName) {
-    this->name = passedName;
+    this.name = *passedName;
 }
 
 /*
@@ -99,6 +104,7 @@ string Food::getName() const {
 
 /*
  * Sets the starting inventory if it is within range [24,99]
+ * int startValue - value for starting inventory
  */
 void Food::setStart(int startValue) {
     if (startValue >= 24 && startValue <= 99) {
@@ -117,6 +123,7 @@ int Food::getStart() const {
 
 /*
  * Sets the value of the ending inventory, as long as it is greater than or equal to 0
+ * int endValue - value for the ending inventory
  */
 void Food::setEnd(int endValue) {
     if (endValue >= 0) {
@@ -135,6 +142,8 @@ int Food::getEnd() const {
 
 /*
  * Sets the value of sales at each index of the salesPerDay array
+ * int index - the day to select
+ * int saleValue - the number of items sold on that day
  */
 void Food::setSales(int index, int saleValue) {
     if (index > numberOfDaysFoodIsSold || index < 0) {
@@ -158,18 +167,20 @@ void Food::showSales() {
 }
 
 /*
- * Displays all available information about the object which it is called upon
+ * Displays all available information about the object when called
+ * Items shown are: Name, starting inventory, sales per day, final inventory amount, and the average sold each day
  */
 void Food::display() {
     cout << "Food Name: " << getName() << endl;
     cout << "Starting inventory: " << getStart() << endl;
     showSales();
+    calcEnd();
     cout << "Final inventory amount: " << Food::getEnd() << endl;
     cout << "The average sold each day is: " << calcAvg() << endl << endl;
 }
 
 /*
- * Destroys the int pointer once the obejct falls out of scope;
+ * Destroys the int pointer once the obejct falls out of scope
  */
 Food::~Food() {
     delete[] salesPerDay;
